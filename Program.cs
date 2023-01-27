0c7c1351881acb7e788f4ed04a2c4b79f08c81ea
Skala-D <S00239867@atu.ie>
2023-01-27 10:53:18 +0000

    Worksheet1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,76 @@
 ﻿namespace Github_Test
 {
+    /* Name: Dominik Skala
+     * Date: 27/01/2023
+     * Desc: Worksheet1 - Q1
+     * 
+     */
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I deleted my Q1 ");
+            Console.WriteLine("\n Q1-A \n");
+            PrintIntegersFor();
+            Console.WriteLine("\n Q1-B \n");
+            PrintIntegersWhile();
+            Console.WriteLine("\n Q1-C \n");
+            PrintIntegersDo();
+            Console.WriteLine("\n Q1-D \n");
+            PrintExcludingFor();
+            Console.WriteLine("\n Q1-E \n");
+            PrintDescendingFor();
         }
+
+        static void PrintIntegersFor()
+        {
+
+            for (int i = 40; i <= 60; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+
+        }
+        static void PrintIntegersWhile()
+        {
+            int integer = 40;
+            while (integer<=60)
+            {
+                Console.WriteLine(integer);
+                integer++;
+            }
+        }
+        static void PrintIntegersDo()
+        {
+            int integer = 40;
+            do
+            {
+                Console.WriteLine(integer);
+                integer++;
+            }
+            while (integer<=60);
+        }
+        static void PrintExcludingFor()
+        {
+            for (int i = 40; i <= 60; i++)
+            {
+                if (i!=46 && i!= 48)
+                Console.WriteLine(i);
+
+            }
+
+        }
+        static void PrintDescendingFor()
+        {
+
+            for (int i = 60; i >= 40; i--)
+            {
+                Console.WriteLine(i);
+
+            }
+
+
+        }
+
     }
 }
---
+++ Question2/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Question2
+{
+    /* Name: Dominik Skala
+     * Date: 27/01/2023
+     * Desc: Worksheet1 - Q2
+     * 
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int count = CountOddNumbers();
+            Console.WriteLine($"{count} total count of odd numbers");
+            AverageMinMax();
+
+        }
+        static int CountOddNumbers()
+        {
+            int[] integers = new int[5];
+
+            int count = 0;
+            
+
+     
+
+            for (int i = 0; i < integers.Length; i++)
+            {
+                Console.WriteLine($"Enter value number {i + 1}");
+                integers[i]= int.Parse(Console.ReadLine());
+                if (integers[i] % 2 == 1 || integers[i]%2==-1)
+                {
+                    count++;
+                }
+            
+            }
+            return count;
+           
+        }
+        static void AverageMinMax()
+        {
+            int average=0;
+
+
+            int[] integers = new int[5];
+            for (int i = 0; i < integers.Length; i++)
+            {
+                Console.WriteLine($"Enter value number {i + 1} : ");
+                integers[i] = int.Parse(Console.ReadLine());
+
+                average = average + integers[i];
+
+            }
+            average = average / integers.Length;
+            int min= integers.Min();
+            int max= integers.Max();
+            Console.WriteLine($"The lowest value: {min} The highest value: {max} The average: {average}");
+
+            }
+    }
+}
